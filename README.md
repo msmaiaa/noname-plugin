0b5a3539037ae70815fc0529d07597a86c2c59b5
msmaiaa <roxx77344@gmail.com>
2022-09-26 16:35:32 -0300

    (plugin): create websocket connection with the server

---
+++ README.md
@@ -0,0 +1,2 @@
+### assets:
+- https://pypi.org/project/websocket-client/ @1.4.1
--- noname.py
+++ noname.py
@@ -1,10 +1,77 @@
 from messages import SayText2
+from listeners.tick import GameThread
+import enum
+import websocket
+import time
+
+
+class GameStatus(enum.Enum):
+    Idle = 0
+    WaitingForPlayers = 1
+    Starting = 2
+    KnifeRound = 3
+    Live = 4
+    Ending = 5
+
+
+GAME_STATUS = GameStatus.Idle
+WS_CONNECTED = False
+WS_CONNECTION = None
+
+
+def update_game_status(status: GameStatus):
+    global GAME_STATUS
+    GAME_STATUS = status
+
+
+class WebSocketRunner():
+    def start_ws(self):
+        global WS_CONNECTION
+        while True:
+            try:
+                WS_CONNECTION = websocket.WebSocketApp(
+                    url="ws://192.168.0.11:1337/ws",
+                    on_close=self.on_ws_close,
+                    on_message=self.on_ws_message,
+                    on_error=self.on_ws_error,
+                    on_open=self.on_ws_open
+                )
+                WS_CONNECTED = True
+                WS_CONNECTION.on_open = self.on_ws_open
+                WS_CONNECTION.run_forever(
+                    skip_utf8_validation=True, ping_interval=10, ping_timeout=8)
+            except Exception as e:
+                WS_CONNECTED = False
+                print("Websocket connection Error  : {0}".format(e))
+                time.sleep(5)
+
+    def on_ws_open(self, ws):
+        print('socket connected')
+        ws.send("Hello")
+
+    def on_ws_close(self, ws, close_status_code, close_msg):
+        global WS_CONNECTED
+        WS_CONNECTED = False
+
+    def on_ws_message(self, ws, message):
+        SayText2(message).send()
+
+    def on_ws_error(self, ws, error):
+        global WS_CONNECTED
+        WS_CONNECTED = False
+        # ws.send("Hello, server")
+
+
+def bootstrap_websocket():
+    ws_runner = WebSocketRunner()
+    thread = GameThread(target=ws_runner.start_ws)
+    thread.daemon = True
+    thread.start()
 
 
 def load():
-    SayText2('Plugin has been loaded successfully!').send()
-    SayText2('Plugin has been loaded successfully!').send()
+    bootstrap_websocket()
 
 
 def unload():
-    SayText2('Plugin has been unloaded successfully!').send()
+    pass
