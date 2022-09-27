9d4d809b4eaa2b259f4a6723441a4c4520023563
msmaiaa <roxx77344@gmail.com>
2022-09-26 23:55:12 -0300

    send server metadata on websocket connection

--- noname.py
+++ noname.py
@@ -3,6 +3,7 @@ from listeners.tick import GameThread
 import enum
 import websocket
 import time
+import json
 
 
 class GameStatus(enum.Enum):
@@ -30,7 +31,7 @@ class WebSocketRunner():
         while True:
             try:
                 WS_CONNECTION = websocket.WebSocketApp(
-                    url="ws://192.168.0.11:1337/ws",
+                    url="ws://192.168.0.11:1337/ws/server",
                     on_close=self.on_ws_close,
                     on_message=self.on_ws_message,
                     on_error=self.on_ws_error,
@@ -46,8 +47,7 @@ class WebSocketRunner():
                 time.sleep(5)
 
     def on_ws_open(self, ws):
-        print('socket connected')
-        ws.send("Hello")
+        refresh_server_status()
 
     def on_ws_close(self, ws, close_status_code, close_msg):
         global WS_CONNECTED
@@ -60,6 +60,23 @@ class WebSocketRunner():
         global WS_CONNECTED
         WS_CONNECTED = False
         # ws.send("Hello, server")
+
+
+def send_ws_msg(action, data):
+    global WS_CONNECTION
+    if WS_CONNECTION is None:
+        return
+    WS_CONNECTION.send(json.dumps({
+        "action": action,
+        "data": data
+    }))
+
+
+def refresh_server_status():
+    global GAME_STATUS
+    send_ws_msg("server_2_backend_update_status", {
+        "status": GAME_STATUS.name
+    })
 
 
 def bootstrap_websocket():
